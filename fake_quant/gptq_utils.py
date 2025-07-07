a4b085c10a6afef3b2272250b957855e633698b1
Prasanth30 <prasanthnaidu31k@gmail.com>
2025-07-07 23:12:52 +0530

    position_id fix

--- fake_quant/gptq_utils.py
+++ fake_quant/gptq_utils.py
@@ -223,7 +223,11 @@ def gptq_fwrd(model, dataloader, dev, args):
             for name in subset:
                 handles.append(subset[name].register_forward_hook(add_batch(name)))
             for j in range(args.nsamples):
-                outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
+                if position_ids is None:
+                    outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
+                else:
+                    outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
+                
             for h in handles:
                 h.remove()
 
