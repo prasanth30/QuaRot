dc8185992ffbc9601f0b626b6bc37419e37ea0a7
Prasanth30 <prasanthnaidu31k@gmail.com>
2025-07-07 23:35:01 +0530

    remove if

--- fake_quant/gptq_utils.py
+++ fake_quant/gptq_utils.py
@@ -223,10 +223,10 @@ def gptq_fwrd(model, dataloader, dev, args):
             for name in subset:
                 handles.append(subset[name].register_forward_hook(add_batch(name)))
             for j in range(args.nsamples):
-                if position_ids is None:
-                    outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
-                else:
-                    outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
+                # if position_ids is None:
+                outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
+                # else:
+                    # outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
                 
             for h in handles:
                 h.remove()
@@ -240,10 +240,10 @@ def gptq_fwrd(model, dataloader, dev, args):
                 gptq[name].free()
 
         for j in range(args.nsamples):
-            if position_ids is None:
-                outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
-            else:
-                outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
+            # if position_ids is None:
+            outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
+            # else:
+                # outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
             # outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, position_ids=position_ids)[0]
 
         layers[i] = layer.cpu()
